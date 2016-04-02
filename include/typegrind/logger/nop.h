9f02b3bf600ef6a4baef8f8f2df3a3d0438a2924
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2016-04-02 18:57:23 +0000

    updated logger api to match the current changes

--- include/typegrind/logger/nop.h
+++ include/typegrind/logger/nop.h
@@ -4,13 +4,13 @@
 
 #define TYPEGRIND_LOG_NEW(locationStr, typeStr, canonicalStr, typeSize, newExpression) (newExpression)
 #define TYPEGRIND_LOG_NEW_ARRAY(locationStr, typeStr, canonicalStr, typeSize, newExpression) (newExpression)
-#define TYPEGRIND_LOG_OP_NEW(typeStr, locationStr, typeSize, size, newExpression) (newExpression)
-#define TYPEGRIND_LOG_OP_NEW_ARRAY(typeStr, locationStr, typeSize, size, newExpression) (newExpression)
+#define TYPEGRIND_LOG_OP_NEW(locationStr, typeStr, canonicalStr, typeSize, size, newExpression) (newExpression)
+#define TYPEGRIND_LOG_OP_NEW_ARRAY(locationStr, typeStr, canonicalStr, typeSize, size, newExpression) (newExpression)
 
 #define TYPEGRIND_LOG_DELETE(locationStr, typeStr, canonicalType, deleteExpression) (deleteExpression)
 #define TYPEGRIND_LOG_DELETE_ARRAYA(locationStr, typeStr, canonicalType, deleteExpression) (deleteExpression)
-#define TYPEGRIND_LOG_OP_DELETE(pointehAddr, locationStr, deleteExpression) (deleteExpression)
-#define TYPEGRIND_LOG_OP_DELETE_ARRAY(pointerAddr, locationStr, deleteExpression) (deleteExpression)
+#define TYPEGRIND_LOG_OP_DELETE(locationStr, typeStr, canonicalType, deleteExpression) (deleteExpression)
+#define TYPEGRIND_LOG_OP_DELETE_ARRAY(locationStr, typeStr, canonicalType, deleteExpression) (deleteExpression)
 
 #define TYPEGRIND_LOG_METHOD_INITIALIZER(targetName, locationStr, customName, flags, initializerExpression) (initializerExpression)
 #define TYPEGRIND_LOG_METHOD_ENTER(targetName, locationStr, customName, flags) /* nothing to do */
